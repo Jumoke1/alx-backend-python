527aef8354aff01f6199fc01420dda88f1fef468
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-05 22:30:53 +0100

    add

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
- function to_kv that takes a string k and an int OR float v as 
+ function to_kv that takes a string k and an int OR float v as
  arguments and returns a tuple.
 '''
 from typing import Union, Tuple
@@ -8,7 +8,7 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
-      Returns a tuple where the first element is the string k and the second
+    Returns a tuple where the first element is the string k and the second
     element is the square of v.
 
     Args:
