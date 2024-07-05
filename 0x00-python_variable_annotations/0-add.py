fd94cb9d29ddbece11f72f24255eb7eb472b8138
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-05 14:26:19 +0100

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+a function to add two float numunders 
+'''
+def add(a:float, b:float) -> float:
+    '''
+    adds two floating-point numbers
+    '''
+    return a + b
