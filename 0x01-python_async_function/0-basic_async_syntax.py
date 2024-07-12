3e4edb4072af9ecf3985e45f4cbb57a9378fc08a
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-12 14:25:57 +0100

    obj

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,12 @@
-##!/usr/bin/env python3
-'''python async.
-'''
-import asyncio
+#!/usr/bin/env python3
+''' python async'''
 import random
+import asyncio
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    '''Waits for a random number of seconds.
-    '''
-    delays = random.random() * max_delay
-    await asyncio.sleep(delays)
-    return delays
+async def wait_random(max_delay: int = 10) ->float:
+    '''Generate a random floating-point number between
+    0 and max_delay (inclusive)'''
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
