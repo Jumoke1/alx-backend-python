c1e08854a43473e7ee013e78d1482c322b03de9c
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-15 16:41:29 +0100

    generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     ''' a coroutine that will loop 10 times and wait 1sec'''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
